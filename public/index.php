6dcf907dab0b1333a542dac0100da5927a367ce5
Abdullahi Kabri <kabriacid01@gmail.com>
2025-01-04 00:37:00 +0100

    Added some background stylings.

--- public/index.php
+++ public/index.php
@@ -5,8 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E - Payment</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="favicon.svg" type="image/x-icon">
 </head>
 <body>
-    
+    <main>
+        <div class="container">
+            <div class="left-content">
+                <div class="rounds">
+                    <div class="round"></div>
+                    <div class="round"></div>
+                    <div class="round"></div>
+                </div>
+                <div class="image-container"></div>
+            </div>
+            <div class="right-content"></div>
+        </div>
+    </main>
 </body>
 </html>
